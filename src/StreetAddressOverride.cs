94928479abb3903e7ded7171c7c423cb45ad8df0
Jake Soenneker <jake@soenneker.com>
2025-05-08 09:06:29 -0500

    Initial commit

---
+++ src/StreetAddressOverride.cs
@@ -0,0 +1,23 @@
+﻿using Soenneker.Dtos.StreetAddress;
+using Soenneker.Utils.AutoBogus.Context;
+using Soenneker.Utils.AutoBogus.Override;
+
+namespace Soenneker.AutoFaker.Overrides.StreetAddresses;
+
+/// <summary>
+/// An AutoFaker (AutoBogus) override for the DTO StreetAddress
+/// </summary>
+public sealed class StreetAddressOverride : AutoFakerOverride<StreetAddress>
+{
+    public override void Generate(AutoFakerOverrideContext context)
+    {
+        var target = (context.Instance as StreetAddress)!;
+
+        target.Line1 = context.Faker.Address.StreetAddress();
+        target.Line2 = context.Faker.Random.Bool() ? context.Faker.Address.SecondaryAddress() : null;
+        target.City = context.Faker.Address.City();
+        target.State = context.Faker.Address.StateAbbr();
+        target.PostalCode = context.Faker.Address.ZipCode();
+        target.Country = "US";
+    }
+}
---
+++ test/Soenneker.AutoFaker.Overrides.StreetAddresses.Tests/StreetAddressOverrideTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.AutoFaker.Overrides.StreetAddresses.Tests;
+
+public sealed class StreetAddressOverrideTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
